b76a138b59bbca6dc529f2efa4b00b89bf5536ca
Stefano Vanin <s.vanin@outlook.it>
2020-09-03 09:08:10 +0200

    section 3 of udemy "Java from Scratch"

---
+++ src/com/SVAX29/Variables.java
@@ -0,0 +1,31 @@
+package com.SVAX29;
+
+public class Variables {
+    public static void main (String args[]){
+        // code goes here
+        System.out.println("Hello World!");
+
+        int a;
+
+        a = 5;
+
+        int b = 4;
+
+        int result = a + b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(result);
+
+        System.out.println("The result is:\t" + result);
+
+        double c = 23.45;
+
+        String word = "Hey";
+
+        String str = "Java is cool";
+
+        System.out.println(str);
+
+    }
+}
