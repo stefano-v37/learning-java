bdc8c2b2fb39b3f546175aaa0776a74017124875
Stefano Vanin <s.vanin@outlook.it>
2020-09-03 10:27:45 +0200

    section 5.1

--- src/com/SVAX29/Variables3.java
+++ src/com/SVAX29/Variables3.java
@@ -4,15 +4,20 @@ public class Variables3 {
 
     int globalV = 4;
 
-    public int reverse (int num){
-        int lastNum = num%10;
-        num = num/10;
-        int midNum = num%10;
-        num = num/10;
-        int firstNum = num%10;
+    public int reverse(int num) {
+        if (num > 99 && num < 1000) {
+            int lastNum = num % 10;
+            num = num / 10;
+            int midNum = num % 10;
+            num = num / 10;
+            int firstNum = num % 10;
 
-        int result = lastNum*100 + midNum*10 + firstNum;
+            int result = lastNum * 100 + midNum * 10 + firstNum;
 
-        return result;
+            return result;
+        } else {
+            System.out.println("You should enter a 3 digits number");
+            return 0;
+        }
     }
 }
