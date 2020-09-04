07eb203064d1f00f699ea43aeaaf4bd368671807
Stefano Vanin <s.vanin@outlook.it>
2020-09-04 12:27:58 +0200

    section 15

--- src/com/SVAX29/Tester5.java
+++ src/com/SVAX29/Tester5.java
@@ -7,8 +7,12 @@ public class Tester5 {
             System.out.println(classA.getStudents()[i].get_testA());
         }
 
+        classA.getStudents()[0].set_name("Lisa");
+
         // if toString method is available the sout will go directly to it when printing the class
         System.out.println(classA.toString());
         System.out.println(classA);
+
+        System.out.println(classA.getStudents()[0].testAvg());
     }
 }
